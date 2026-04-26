23115908a81056fe07d86d4940c4507a0a95654d
maryam.f <maryam.f@smartech.ir>
2026-04-26 10:33:12 +0330

    chore: initialize new project with base structure

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className=" flex items-center justify-center gap-2 mt-10">
+      <h1 className="text-red-900 text-3xl  ">Quiz TS - maryam farhadi</h1>
+    </div>
+  );
+}
